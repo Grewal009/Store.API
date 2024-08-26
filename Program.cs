99081f58c4c24914039acf839f8f90672217fb98
Aneel Grewal <aneel.grewal@hotmail.com>
2024-08-26 06:18:22 +0200

    improve code by declaring const GetGameEndpointName

--- Program.cs
+++ Program.cs
@@ -4,6 +4,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 var app = builder.Build();
 
+const string GetGameEndpointName = "GetGame";
+
 List<GameDto> games = [
     new(1, "Street Fighter","Fighting",19.99M,new DateOnly(2020,10,15)),
     new(2, "Final Fantasy","Fantasy",29.99M,new DateOnly(2022,1,22)),
@@ -12,7 +14,7 @@ List<GameDto> games = [
 
 app.MapGet("games", () => games);
 
-app.MapGet("games/{id}", (int id) => games.Find(x => x.Id == id)).WithName("GetGame");
+app.MapGet("games/{id}", (int id) => games.Find(x => x.Id == id)).WithName(GetGameEndpointName);
 //give name to the endpoint to access the created resource .WithName("GetGame")
 
 app.MapPost("games", (CreateGameDto newGame) =>
@@ -26,7 +28,7 @@ app.MapPost("games", (CreateGameDto newGame) =>
     );
     games.Add(game);
 
-    return Results.CreatedAtRoute("GetGame", new { id = game.Id }, game);
+    return Results.CreatedAtRoute(GetGameEndpointName, new { id = game.Id }, game);
 });
 
 app.Run();
