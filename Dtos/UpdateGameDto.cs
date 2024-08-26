fd923b9bff0a23e2804bb6c0aa824628461ca073
Aneel Grewal <aneel.grewal@hotmail.com>
2024-08-26 23:16:29 +0200

    validation added using annotations and MinimalApis.Extensions nuget package

--- Dtos/UpdateGameDto.cs
+++ Dtos/UpdateGameDto.cs
@@ -1,8 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+
 namespace Store.API.Dtos;
 
 public record class UpdateGameDto(
-    string Name,
-    string Genre,
-    decimal Price,
+    [Required][StringLength(50)] string Name,
+    [Required][StringLength(20)] string Genre,
+    [Range(1, 100)] decimal Price,
     DateOnly ReleaseDate
 );
