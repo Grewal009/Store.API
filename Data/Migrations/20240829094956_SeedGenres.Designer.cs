33c862f6a03f0a048c8fb3fd14404ad3b9acfe7d
Aneel Grewal <aneel.grewal@hotmail.com>
2024-08-29 11:54:14 +0200

    seeding data(static) into Genres table

--- Data/Migrations/20240829094956_SeedGenres.Designer.cs
+++ Data/Migrations/20240829094956_SeedGenres.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Store.API.Data;
 
@@ -10,9 +11,11 @@ using Store.API.Data;
 namespace Store.API.Data.Migrations
 {
     [DbContext(typeof(StoreContext))]
-    partial class StoreContextModelSnapshot : ModelSnapshot
+    [Migration("20240829094956_SeedGenres")]
+    partial class SeedGenres
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "9.0.0-preview.7.24405.3");
